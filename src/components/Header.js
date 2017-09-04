94219bcbbbd0b22a3b05d931a2b541783aaf4524
Miguel Siroit <msiroit@velocitypartners.net>
2017-09-04 17:29:51 -0300

    Actualizacion de GUI: Se agregó boton Home y se ocultan botones una ves accedida una opción

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import "../css/DondeAyudar.css"
 import "../css/AquiSeNecesita.css"
 import "../css/Logo.css"
 import logo from "../img/Go-HELP.png"
-const Header = () => (
+import home from "../img/home.png"
+const Header = (props) => (
   <header>
-    <nav>
-		<img alt="go-help" className="img-logo" src={logo} />
+    <nav className={props.location.pathname.replace('/','') || 'home'}>
+		<Link className="button-home" to='/'>
+			<img alt="home" className="img-home" src={home} />
+		</Link>
+			<img alt="go-help" className="img-logo" src={logo} />
 		<Link className="button-da" to='/donde-ayudar'>Donde ayudar?</Link>
 		<Link className="button-asn" to='/aqui-se-necesita'>Aquí se necesita</Link>
 	</nav>
   </header>
 )
 
-export default Header
+export default withRouter(props => <Header {...props}/>)
